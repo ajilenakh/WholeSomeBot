5165fb8bd737f31b5ab75bcc00c6343a04e3a1cc
Arindam <107887918+Arindam905@users.noreply.github.com>
2023-09-15 18:55:05 +0530

    removed music.py, optimised waifu code

--- cogs/waifu.py
+++ cogs/waifu.py
@@ -7,33 +7,45 @@ from nextcord.ext import commands
 
 load_dotenv()
 
-def input(msg, author):
+def api_request(msg, author, userName):
     url = "https://waifu.p.rapidapi.com/path"
 
-    querystring = {"user_id":f"{author}","message":f"{msg}","from_name":f"{userName[:-5]}","to_name":"Girl","situation":"Girl loves "+f"{userName[:-5]}"+" .","translate_from":"auto","translate_to":"eng"}
+    payload = {
+        "user_id": str(author),
+        "message": msg,
+        "from_name": str(userName),
+        "to_name": "Girl",
+        "situation": f"Girl loves {userName}.",
+        "translate_from": "auto",
+        "translate_to": "eng"
+    }
 
-    payload = {}
     headers = {
-        "content-type": "application/json",
         "X-RapidAPI-Key": os.getenv("waifu_key"),
         "X-RapidAPI-Host": "waifu.p.rapidapi.com"
     }
 
-    response = requests.request("POST", url, json=payload, headers=headers, params=querystring)
+    try:
+        response = requests.post(url, headers=headers, params=payload)
+        response.raise_for_status()  # Raise an exception if the request was not successful
 
-    return(response.text)
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        # Handle any request-related exceptions here
+        print(f"Error: {e}")
+        return {"error": "Request failed"}
 
 class waifu_cog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-    
+
     @nextcord.slash_command(guild_ids=[1062474098418139256])
     async def waifu(self, interaction: nextcord.Interaction, msg:str):
-        global userName
-        author = interaction.user
-        userName= (f"{author}")
-        await interaction.response.send_message(f"{userName[:-5]}: {msg}")
-        await interaction.followup.send("Waifu: " + input(msg, author))
+        userName = str(interaction.user)
+        waifu_response = api_request(msg, interaction.user.id, userName)
+        print("Waifu API Response:", waifu_response) 
+        await interaction.response.send_message(f"{userName}: {msg}")
+        await interaction.followup.send(f"Waifu: {waifu_response}")
         
 def setup(bot):
     bot.add_cog(waifu_cog(bot))
